7f34c73c314261f87072705d411e90f560100681
Happylilly1231 <happylilly2017@gmail.com>
2024-05-26 04:20:34 +0900

    답변 댓글&대댓글 추가

--- cmapp/models.py
+++ cmapp/models.py
@@ -28,7 +28,7 @@ class Answer(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comment')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    # answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='answer_comment')
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True)
     parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name='recomment')
     content = models.TextField(max_length=200)
     create_date = models.DateTimeField()
